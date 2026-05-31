40f568f1ffd5e7d4a541b2ad5d3e763bb225a67b
Shashwat Gupta <shashwat@gmail.com>
2026-05-31 16:32:23 +0530

    added routing functionality

--- Book-managementSystem/src/App.jsx
+++ Book-managementSystem/src/App.jsx
@@ -2,6 +2,8 @@ import Booklist from "./components/BookList";
 import { Books, } from "./assets/utils/Mockdata";
 import "./components/style.css";
 import { useState } from "react";
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
 
 
 function App() {
@@ -12,16 +14,19 @@ function App() {
     const filterBooks = Books.filter((book) => book.title.toLowerCase().includes(searchText.toLowerCase()));
     console.log("filteredBooks", filteredBooks);
     setFilteredBooks(filterBooks);  
+    
 
   }
   return (
     <>
+    <Header/>
       <div className="search">
         <h2>Search Books</h2>
         <input type="text" className="search-input" onChange={(e) => setSearchText(e.target.value)} />
         <button onClick={handleSearch}>Search</button>
       </div>
       <Booklist booksData={filteredBooks} />
+      <Outlet/>
     </>
 
   );
